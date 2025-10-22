f53ab5d0a1f5cedd73b8e5ca85a987d1de57cf57
ducb2303807 <ducb2303807@student.ctu.edu.vn>
2025-10-22 18:12:52 +0700

    clone and update from BE1-CT449

---
+++ app/services/contact.service.js
@@ -0,0 +1,44 @@
+const { ObjectId } = require('mongodb');
+
+class ContactService {
+    constructor(client) {
+        this.Contact = client.db().collection("Contacts");
+    }
+    // định nghĩa thêm các thương thức truy xuất CSDL ở dưới
+    extractContactData(payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+        // remove undefined fields
+        Object.keys(contact).forEach(
+            (key) => contact[key] == undefined && delete contact[key]
+        );
+        return contact;
+    }
+
+    async create(payload) {
+        const contact = this.extractContactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            {$set: { favorite: contact.favorite === true } },
+            { returnDocument: "after", upsert: true }
+        );
+        return result;
+    }
+
+    async find(filter) {
+        const cursor = await this.Contact.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find( {
+            name: { $regex: new RegExp(name), $options: "i" },
+        });
+    }
+}
+module.exports = ContactService;
