1ac9b00b1fb556520905419ad97e1d49b4db2064
ducb2303807 <ducb2303807@student.ctu.edu.vn>
2025-10-23 08:33:51 +0700

    Cai dat cac handler truy xuat CSDL

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -15,7 +15,7 @@ class ContactService {
         };
         // remove undefined fields
         Object.keys(contact).forEach(
-            (key) => contact[key] == undefined && delete contact[key]
+            (key) => contact[key] === undefined && delete contact[key]
         );
         return contact;
     }
@@ -39,6 +39,41 @@ class ContactService {
         return await this.find( {
             name: { $regex: new RegExp(name), $options: "i" },
         });
+    }
+
+    async findById(id) {
+        return await this.Contact.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        };
+        const update = this.extractContactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { returnDocument: "after" }
+        );
+        return result;
+    }
+
+    async delete(id) {
+        const result = await this.Contact.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+        return result;
+    }
+
+    async findFavorite() {
+        return await this.find({ favorite: true });
+    }
+
+    async deleteAll() {
+        const result = await this.Contact.deleteMany({});
+        return result;
     }
 }
 module.exports = ContactService;
